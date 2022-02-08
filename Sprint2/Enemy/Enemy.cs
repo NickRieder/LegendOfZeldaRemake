754d3cf7d49f70c5b1d6844f9ecdfb9c8e0613a0
Kevin Nguyen <kevinnguyen0811@gmail.com>
2022-02-08 17:01:24 -0500

    Created IEnemyState interface, Enemy class, and DownMovingEnemyState class.

---
+++ Sprint2/Enemy/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint2.Enemy
+{
+    public class Enemy
+    {
+        public IEnemyState enemyState;
+
+        public Enemy()
+        {
+            enemyState = new DownMovingEnemyState(this);
+        }
+
+        // Enemy Move
+        public void MoveLeft()
+        {
+            enemyState.MoveLeftState();
+        }
+        public void MoveRight()
+        {
+            enemyState.MoveRightState();
+        }
+        public void MoveUp()
+        {
+            enemyState.MoveUpState();
+        }
+        public void MoveDown()
+        {
+            enemyState.MoveDownState();
+        }
+
+        // Enemy Attack
+        public void AttackLeft()
+        {
+            enemyState.AttackleftState();
+        }
+        public void AttackRight()
+        {
+            enemyState.AttackRightState();
+        }
+        public void AttackUp()
+        {
+            enemyState.AttackUpState();
+        }
+        public void AttackDown()
+        {
+            enemyState.AttackDownState();
+        }
+
+        // Enemy Take Damage
+        public void TakeDamageLeft()
+        {
+
+        }
+        public void TakeDamageRight()
+        {
+
+        }
+        public void TakeDamageUp()
+        {
+
+        }
+        public void TakeDamageDown()
+        {
+
+        }
+
+    }
+}
