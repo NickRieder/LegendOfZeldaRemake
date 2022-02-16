4361eff761c66b7d0d368178b39bc75bf73c807c
theyajdhawan <dhawan.yajat@gmail.com>
2022-02-15 19:18:25 -0500

    * BombItem.cs: new items * IItem.cs: * BoomerangItemOne.cs: * BoomerangItemTwo.cs: * ExplosionItemOne.cs: * ExplosionItemTwo.cs: * BoomerangItemThree.cs: * ExplosionItemThree.cs:

---
+++ Sprint2/Items/IItem.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Sprint2.Items
+{
+    public interface IItem
+    {
+        Texture2D Texture { get; set; }
+        void Update();
+        Rectangle MakeDestinationRectangle(Vector2 location);
+        void Draw(SpriteBatch spriteBatch, Vector2 location);
+    }
+}
