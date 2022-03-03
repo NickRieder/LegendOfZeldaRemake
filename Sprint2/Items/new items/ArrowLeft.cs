380d0f26e41bd75eea102a9a4b188aa7b1c71004
Zachary Richter <richterzach15@gmail.com>
2022-03-02 22:26:28 -0500

    Created sprite class, implemetned new sprite stuff for items (still working on explosion)

--- Sprint2/Items/new items/ArrowLeft.cs
+++ Sprint2/Items/new items/ArrowLeft.cs
@@ -6,26 +6,24 @@ namespace Sprint2
 {
     public class ArrowLeft : IItem
     {
-        private Rectangle frame1;
-        private Texture2D sheet;
         private Vector2 itemPos;
+        private Sprite arrow;
 
         public ArrowLeft(Link link, SpriteFactory spriteFactory)
         {
-            frame1 = SpriteFactory.ARROW_UPSIDEDOWN_DOWN;
-            this.sheet = spriteFactory.getLinkSheet();
+            arrow = spriteFactory.getArrowSpriteLeft();
             itemPos.X = link.pos.X;
             itemPos.Y = link.pos.Y;
         }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 location)
         {
-            Rectangle destinationRectangleFrame1 = new Rectangle((int)itemPos.X, (int)itemPos.Y, frame1.Width, frame1.Height);
-            spriteBatch.Draw(sheet, destinationRectangleFrame1, frame1, Color.White);
+            arrow.Draw(spriteBatch, itemPos);
         }
 
-        public void Update()
+        public void Update(GameTime gameTime)
         {
+            arrow.Update(gameTime);
             itemPos.X -= 5;
         }
     }
