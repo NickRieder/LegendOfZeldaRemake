63fb13f583bc8aaa494dad4d8687eac0b72bf4a4
theyajdhawan <dhawan.yajat@gmail.com>
2022-04-08 09:37:57 -0400

    winningstate

--- Sprint2/GameStates/WinningState.cs
+++ Sprint2/GameStates/WinningState.cs
@@ -15,13 +15,14 @@ namespace Sprint2.GameStates
         private SpriteFont font;
         private bool previously_start;
         private bool previously_select;
+        private bool isStartPressed;
 
         public WinningState(Game1 game, SpriteFont font)
         {
             this.game = game;
             this.font = font;
             cursorpos = 0;
-            previously_select = previously_select = false;
+            previously_select = previously_select = isStartPressed = false;
         }
 
         private void MoveCursor()
@@ -30,39 +31,29 @@ namespace Sprint2.GameStates
             cursorpos %= 2;
         }
 
-        private void Confirm()
+        private void Confirm(bool start_pressed)
         {
-            switch (cursorpos)
+            switch (start_pressed)
             {
-                case 0:
+                case true:
                     Thread.Sleep(500);
                     new ResetGame(game).Execute();
                     break;
-                case 1:
+                case false:
                     game.Exit();
                     break;
             }
         }
 
-        public void Draw(SpriteBatch sb)
-        {
-            sb.DrawString(font, "Thanks Link, You're", new Vector2(20, 30), Color.Red);
-            sb.DrawString(font, "The Hero of Hyrule.", new Vector2(50, 80), Color.Red);
-            sb.DrawString(font, "Finally", new Vector2(50, 130), Color.Red);
-            sb.DrawString(font, "Peace returns to Hyrule.", new Vector2(50, 180), Color.Red);
-            sb.DrawString(font, "This ends the story.", new Vector2(20, 80 + 50 * cursorpos), Color.Blue);
-            sb.DrawString(font, "Another story awaits! Press the Start button.", new Vector2(50, 230), Color.Red);
-        }
-
         public void Update()
         {
-            bool select_pressed = Keyboard.GetState().IsKeyDown(Keys.RightShift);
-            bool start_pressed = Keyboard.GetState().IsKeyDown(Keys.Enter);
             GamePadState state = GamePad.GetState(PlayerIndex.One);
+            bool select_pressed = Keyboard.GetState().IsKeyDown(Keys.RightShift);
+            bool start_pressed = Keyboard.GetState().IsKeyDown(Keys.Enter) || state.IsButtonDown(Buttons.Start);
 
             if ((start_pressed && !previously_start) || state.IsButtonDown(Buttons.Start))
             {
-                Confirm();
+                Confirm(start_pressed);
                 return;
             }
 
@@ -73,6 +64,16 @@ namespace Sprint2.GameStates
 
             previously_start = start_pressed;
             previously_select = select_pressed;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.DrawString(font, "Thanks Link, You're", new Vector2(50, 30), Color.Red);
+            sb.DrawString(font, "The Hero of Hyrule.", new Vector2(50, 80), Color.Red);
+            sb.DrawString(font, "Finally", new Vector2(50, 130), Color.Red);
+            sb.DrawString(font, "Peace returns to Hyrule.", new Vector2(50, 180), Color.Red);
+            sb.DrawString(font, "This ends the story.", new Vector2(50, 205), Color.Blue);
+            sb.DrawString(font, "Another story awaits! Press the Start button.", new Vector2(50, 230), Color.Red);
         }
     }
 }
